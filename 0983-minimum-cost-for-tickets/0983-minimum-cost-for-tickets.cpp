37c73bb3f170d39df0df000146d1a5218eceb55d
Qureshi Mohammad Ahtisham <maq.06169@gmail.com>
2026-07-02 01:23:35 +0530

    Time: 2 ms (31.55%), Space: 14.5 MB (14.42%) - LeetHub

---
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int solve(int i, vector<int>& days, vector<int>& costs, vector<int>& dp) {
+        if (i >= days.size())
+            return 0;
+
+        if (dp[i] != -1)
+            return dp[i];
+
+        int oneDay = costs[0] + solve(i + 1, days, costs, dp);
+
+        int j = i;
+        while (j < days.size() && days[j] < days[i] + 7)
+            j++;
+        int sevenDay = costs[1] + solve(j, days, costs, dp);
+
+        j = i;
+        while (j < days.size() && days[j] < days[i] + 30)
+            j++;
+        int thirtyDay = costs[2] + solve(j, days, costs, dp);
+
+        return dp[i] = min({oneDay, sevenDay, thirtyDay});
+    }
+
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        vector<int> dp(days.size(), -1);
+        return solve(0, days, costs, dp);
+    }
+};
