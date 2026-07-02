e5ee0440a0563f44b07bb34302a6ff6672c77835
Qureshi Mohammad Ahtisham <maq.06169@gmail.com>
2026-07-02 18:06:29 +0530

    Implement space-optimized solution for maximal square

---
+++ 0221-maximal-square/SpaceOptimized.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        vector<int> dp(n, 0);
+        int maxi = 0;
+
+        for (int i = 0; i < m; i++) {
+            vector<int> curr(n,0);
+            for (int j = 0; j < n; j++) {
+
+                if (matrix[i][j] == '0')
+                    curr[j] = 0;
+
+                else if (i == 0 || j == 0)
+                    curr[j] = 1;
+
+                else
+                    curr[j] = 1 + min({dp[j], curr[j-1], dp[j-1]});
+
+                maxi = max(maxi, curr[j]);
+            }
+            dp = curr;
+        }
+
+        return maxi * maxi;
+    }
+};
