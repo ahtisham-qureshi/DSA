32585c69207de5f8621ca222e178f5fa74e491ea
Qureshi Mohammad Ahtisham <maq.06169@gmail.com>
2026-07-07 22:02:31 +0530

    Implement tabulation method for coin change problem

---
+++ 0518-coin-change-ii/tabulation.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    vector<vector<long long>> dp;
+
+public:
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        dp.assign(n, vector<long long>(amount + 1, 0));
+
+        for(int i = 0;i<n;i++) dp[i][0] = 1;
+        for(int i = coins[0];i<=amount;i += coins[0]) dp[0][i] = 1;
+
+        for(int i = 1;i<n;i++){
+            for(int target = 1;target<=amount;target++){
+                long long skip = dp[i - 1][target];
+                long long take = 0;
+                if (coins[i] <= target)
+                    take = dp[i][target - coins[i]];
+
+                dp[i][target] = skip + take;
+            }
+        }
+
+
+        return (int)dp[n-1][amount];
+    }
+};
