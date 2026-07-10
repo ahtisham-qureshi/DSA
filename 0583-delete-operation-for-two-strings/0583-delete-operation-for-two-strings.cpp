4b3a06617b8a2fcf31b7e4cff457c8e6cfc0ea73
Qureshi Mohammad Ahtisham <maq.06169@gmail.com>
2026-07-10 22:22:59 +0530

    Time: 17 ms (18.3%), Space: 17.2 MB (10.38%) - LeetHub

---
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    vector<vector<int>> dp;
+    int solve(int i, int j, string& text1, string& text2) {
+        if (i < 0 || j < 0)
+            return 0;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        if (text1[i] == text2[j]) {
+            return dp[i][j] = 1 + solve(i - 1, j - 1, text1, text2);
+        }
+
+        return dp[i][j] = max(solve(i - 1, j, text1, text2),
+                              solve(i, j - 1, text1, text2));
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.length();
+        int n = text2.length();
+        dp.assign(m, vector<int>(n, -1));
+
+        return solve(m - 1, n - 1, text1, text2);
+    }
+public:
+    int minDistance(string word1, string word2) {
+        int lcs = longestCommonSubsequence(word1,word2);
+        return (word1.length() + word2.length()) - 2*lcs;
+    }
+};
