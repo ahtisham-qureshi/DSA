fbf44df5928dad56f56e42eb5053626e08f86978
Qureshi Mohammad Ahtisham <maq.06169@gmail.com>
2026-07-12 10:52:57 +0530

    Time: 34 ms (84.43%), Space: 10.4 MB (84.28%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m = s.length();
+        int n = p.length();
+
+        vector<bool> prev(n + 1, false), curr(n+1,false);
+
+        prev[0] = true;
+        for (int k = 1; k <= n; k++) {
+            if (prev[k - 1] && p[k - 1] == '*')
+                prev[k] = true;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            curr[0] = false;
+            for (int j = 1; j <= n; j++) {
+                if (s[i - 1] == p[j - 1] || p[j - 1] == '?') {
+                    curr[j] = prev[j - 1];
+                } else if (p[j - 1] == '*') {
+                    curr[j] = prev[j] || curr[j - 1];
+                } else
+                    curr[j] = 0;
+            }
+            prev = curr;
+        }
+
+        return prev[n];
+    }
+};
