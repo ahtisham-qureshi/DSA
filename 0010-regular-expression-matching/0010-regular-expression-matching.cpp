a5e873aa85f447836d474246707f27a2301b3e59
Qureshi Mohammad Ahtisham <maq.06169@gmail.com>
2026-07-12 12:59:54 +0530

    Time: 0 ms (100%), Space: 8.2 MB (85.41%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,30 +1,33 @@
 class Solution {
-    vector<vector<bool>> dp;
 
 public:
     bool isMatch(string s, string p) {
         int m = s.length();
         int n = p.length();
 
-        dp.assign(m + 1, vector<bool>(n + 1, false));
+        vector<bool> prev(n + 1, false), curr(n + 1, false);
 
-        dp[0][0] = true;
+        prev[0] = true;
         for (int k = 2; k <= n; k += 2) {
             if (p[k - 1] == '*')
-                dp[0][k] = dp[0][k - 2];
+                prev[k] = prev[k - 2];
         }
 
         for (int i = 1; i <= m; i++) {
+            curr[0] = false;
             for (int j = 1; j <= n; j++) {
                 if (s[i - 1] == p[j - 1] || p[j - 1] == '.') {
-                    dp[i][j] = dp[i - 1][j - 1];
+                    curr[j] = prev[j - 1];
                 } else if (p[j - 1] == '*') {
                     int match = (p[j - 2] == s[i - 1] || p[j - 2] == '.');
-                    dp[i][j] = dp[i][j - 2] || (match && dp[i - 1][j]);
+                    curr[j] = curr[j - 2] || (match && prev[j]);
+                }else {
+                    curr[j] = false;
                 }
             }
+            prev = curr;
         }
 
-        return dp[m][n];
+        return prev[n];
     }
 };
