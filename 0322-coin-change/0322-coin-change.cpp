61bf301975cd388ac98fb0ada5d8065df505ba81
Mohammad Ahtisham Qureshi <maq.06169@gmail.com>
2026-09-20 14:19:27 +0530

    Time: 13 ms (95.02%), Space: 17.8 MB (93.92%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,27 +1,25 @@
 class Solution {
     const int INF = 1e9;
-    vector<vector<int>> dp;
+    vector<int> dp;
 
 public:
-    int solve(int i,int target, vector<int>& coins) {
-        if (target < 0 || i<0) return INF;
-        if (target == 0) return 0;
-
-        if (dp[i][target] != -1)
-            return dp[i][target];
-
-        int best = INF;
-
-        int take = 1 + solve(i,target-coins[i],coins);
-        int skip = solve(i-1,target,coins);
-
-        return dp[i][target] = min(take,skip);
-    }
-
     int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        dp.assign(n,vector<int>(amount + 1, -1));
-        int ans = solve(n-1,amount, coins);
-        return ans == INF ? -1 : ans;
+        dp.assign(amount + 1, INF);
+
+        dp[0] = 0;
+
+        for (int i = 1; i <= amount; i++) {
+            int best = INF;
+            
+            for (int coin : coins)
+                if(i>= coin)
+                    best = min(best, dp[i - coin]);
+
+            if (best != INF)
+                dp[i] = 1 + best;
+        }
+
+
+        return dp[amount] == INF ? -1 : dp[amount];
     }
 };
